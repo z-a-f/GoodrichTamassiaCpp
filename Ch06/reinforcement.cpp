90e4f8c8570d26d42a4573a6ec9e0f1c929d3c7f
Zafar Takhirov <z.tahirov@gmail.com>
2014-09-21 23:22:05 -0700

    AUTOCOMMIT: Sun Sep 21 23:22:05 PDT 2014

--- Ch06/reinforcement.cpp
+++ Ch06/reinforcement.cpp
@@ -1,5 +1,7 @@
 #include <cmath>
 #include <iostream>
+#include <random>
+#include <ctime>
 
 using namespace std;
 
@@ -29,13 +31,115 @@ void reverseArray(T *ar, size_t n) {
 }
 
 void R_6_1() {
+	size_t len;
+
+	cout << "Test ODD array:\n";
+	len = 9;
 	int A[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-	print<int>(A, 9);
-	reverseArray<int>(A, 9);
-	print<int>(A, 9);
+	print<int>(A, len);
+	reverseArray<int>(A, len);
+	print<int>(A, len);
+	
+	cout << "Test EVEN array:\n";
+	len = 10;
+	int B[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	print<int>(B, len);
+	reverseArray<int>(B, len);
+	print<int>(B, len);
+
+	cout << "Test EMPTY array\n";
+	len = 0;
+	int C[] = {};
+	print<int>(C, len);
+	reverseArray<int>(C, len);
+	print<int>(C, len);
+}
+
+// R-6.2
+template <typename T>
+void permuteArray(T *ar, size_t n) {
+	for (size_t i = 0; i < n; i++) {
+		swap(ar[i], ar[rand() % (n - i) + i]);
+	}
+}
+
+void R_6_2() {
+	size_t len;
+
+	cout << "Test ODD array:\n";
+	len = 9;
+	int A[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	print<int>(A, len);
+	permuteArray<int>(A, len);
+	print<int>(A, len);
+	
+	cout << "Test EVEN array:\n";
+	len = 10;
+	int B[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	print<int>(B, len);
+	permuteArray<int>(B, len);
+	print<int>(B, len);
+
+	cout << "Test EMPTY array\n";
+	len = 0;
+	int C[] = {};
+	print<int>(C, len);
+	permuteArray<int>(C, len);
+	print<int>(C, len);
+}
+
+// R-6.3
+template <typename T>
+void rotateArray(T *ar, int d, size_t n) {
+	T temp[n];
+	if (d > 0) {
+		for (size_t i = 0; i < d; i++) {
+			temp[i] = ar[n-d+i];
+		}
+		for (size_t i = 0; i < n-d; i++) {
+			temp[i+d] = ar[i];
+		}
+	} else if (d < 0) {
+		for (size_t i = 0; i < n-d; i++) {
+			temp[i] = ar[i-d];	// Note that d<0
+		}
+		for (size_t i = 0; i < d; i++) {
+			temp[i+n-d] = ar[i+n-d];
+		}
+	} else return;
+}
+
+void R_6_3 () {
+	size_t len;
+
+	cout << "Test ODD array:\n";
+	len = 9;
+	int A[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	print<int>(A, len);
+	rotateArray<int>(A, 1, len);
+	print<int>(A, len);
+	
+	cout << "Test EVEN array:\n";
+	len = 10;
+	int B[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	print<int>(B, len);
+	rotateArray<int>(B, 1, len);
+	print<int>(B, len);
+
+	cout << "Test EMPTY array\n";
+	len = 0;
+	int C[] = {};
+	print<int>(C, len);
+	rotateArray<int>(C, 1, len);
+	print<int>(C, len);
 }
 
 int main() {
+	srand(time(NULL));
 	cout << "***************R-6.1***************\n";
 	R_6_1();
+	cout << "***************R-6.2***************\n";
+	R_6_2();
+	cout << "***************R-6.3***************\n";
+	R_6_3();
 }
