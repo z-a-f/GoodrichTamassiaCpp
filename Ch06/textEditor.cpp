26e7a31befd9b39d0b2f51fba0efc778b8825ba0
Zafar Takhirov <cc.rafaz@zafar.cc>
2014-11-06 15:30:42 -0800

    AUTOCOMMIT(ztakhiro): Thu Nov  6 15:30:42 PST 2014

--- Ch06/textEditor.cpp
+++ Ch06/textEditor.cpp
@@ -1,16 +1,29 @@
 
 #include <iostream>
 
-#include "List.hpp" // Provides the doubly linked list
+#include "ListTemplated.hpp" // Provides the doubly linked list
 
 using namespace std;
 
-/*
- * List declaration
- */
+void updateScreen(NodeList<char> NL) {
+  for (NodeList<char>::Iterator it = NL.begin(); it != NL.end(); ++it) {
+    std::cout << *it;
+  }
+}
 
-
+void getKey(NodeList<char> NL) {
+  char c;
+  std::cin >> c;
+  if (c == char("\u2192")) {
+    std::cout << "TEST";
+  }
+  NL.insert(NL.position(), c);
+}
 
 int main() {
-  NodeList a;
+  NodeList<char> text;
+  while (true) {
+    updateScreen(text);
+    getKey(text);
+  }
 }
