f07bcdd353a705811df62921f64fc4ce95360c88
Zafar Takhirov <z.tahirov@gmail.com>
2014-12-22 02:15:57 -0800

    AUTOCOMMIT(ZafarTakhirov): Mon Dec 22 02:15:57 PST 2014

--- Ch07/testTree.cpp
+++ Ch07/testTree.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
-#include "linkedTree.hpp"
+// #include "linkedTree.hpp"
 // #include "Position.hpp"
+#include "treeGeneric.hpp"
 
 using namespace std;
 
@@ -24,4 +25,34 @@ int main() {
 
   lbt.expandExternal(lbt.root());
   cout << lbt.empty() << endl;
+  cout << lbt.size() << endl;
+
+  try {
+    lbt.expandExternal(lbt.root());
+    cout << lbt.empty() << endl;
+    cout << lbt.size() << endl;
+  } catch (RuntimeException e) {
+    cout << e.getMessage() << endl;
+    // cout << "Error\n";
+  }
+
+  cout << "root->right is external: " << lbt.root().right().isExternal() << endl;
+  try {
+    lbt.expandExternal(lbt.root().right());
+  } catch (RuntimeException e) {
+    cout << e.getMessage() << endl;
+  }
+
+  try {
+    lbt.removeAboveExternal(lbt.root().left());
+  } catch (RuntimeException e) {
+    cout << e.getMessage() << endl;
+  }
+  cout << lbt.empty() << endl;
+  cout << lbt.size() << endl;
+
+  LinkedBinaryTree<int>::PositionList pl;
+  pl = lbt.positions();
+
+
 }
